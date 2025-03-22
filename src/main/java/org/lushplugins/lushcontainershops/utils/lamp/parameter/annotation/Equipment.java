bc7ac75323914750fcbdbe12fcfc628a305ed2d5
OakLoaf <oak@beaconstudios.org>
2025-03-22 21:49:44 +0000

    Fixed annotations

--- src/main/java/org/lushplugins/lushcontainershops/utils/lamp/parameter/annotation/Equipment.java
+++ src/main/java/org/lushplugins/lushcontainershops/utils/lamp/parameter/annotation/Equipment.java
@@ -2,6 +2,10 @@ package org.lushplugins.lushcontainershops.utils.lamp.parameter.annotation;
 
 import org.bukkit.inventory.EquipmentSlot;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Equipment {
     EquipmentSlot value();
 }
--- src/main/java/org/lushplugins/lushcontainershops/utils/lamp/parameter/annotation/RayTrace.java
+++ src/main/java/org/lushplugins/lushcontainershops/utils/lamp/parameter/annotation/RayTrace.java
@@ -1,5 +1,12 @@
 package org.lushplugins.lushcontainershops.utils.lamp.parameter.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
 public @interface RayTrace {
     int distance() default 5;
 }
