14cdd5f0337689241c32b10547ee9306de739d9a
OakLoaf <oak@beaconstudios.org>
2025-03-26 14:34:12 +0000

    Removed empty line

--- src/main/java/org/lushplugins/lushcontainershops/listener/SignListener.java
+++ src/main/java/org/lushplugins/lushcontainershops/listener/SignListener.java
@@ -67,7 +67,6 @@ public class SignListener implements Listener {
             LushContainerShops.getInstance().callEvent(new ShopSignPrepareEvent(shop, ShopSignPrepareEvent.Step.ADD_COST));
             shop.updateSignState();
             LushContainerShops.getInstance().getConfigManager().sendMessage(player, "updated-shop");
-
             return;
         }
 
